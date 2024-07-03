78d3297603cd61245eedca8e9e4176b64a8e344b
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-07-03 10:57:23 +0200

    v

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Book } from '../model/Book';
 import {MatCardModule} from '@angular/material/card';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [MatCardModule,CommonModule],
+  imports: [MatCardModule,CommonModule,MatIconModule,MatButtonModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
